56646c0bd58907a995166b68aa107243e7df8d1e
Winney360 <nkathawinnie94@gmail.com>
2025-07-18 10:50:00 +0300

    modified server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:5173'], // Match Vite port
+  origin: ['https://mern-week-7.vercel.app/'], // Match Vite port
   credentials: true,
 }));
 app.use(express.json());
